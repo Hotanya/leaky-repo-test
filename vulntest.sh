5c42544830f6cb47f1375b847129b7c2fc90845a
ninjat <hotanya.r@gmail.com>
2026-04-15 13:53:30 +1200

    Update vulntest.sh

--- vulntest.sh
+++ vulntest.sh
@@ -4,3 +4,4 @@ echo "This is a test script to check github workflow for gitleaks scanning"
 
 export PASS='totallfakepassword'
 export anotherpass='$p3c1alP@$$w0RDtesting23&^R'
+var apiKey = "SG.xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
